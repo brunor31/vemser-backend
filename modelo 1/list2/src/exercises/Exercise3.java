e5b4f0b24355e340f86cdb0b20cce4a3581b025e
Bruno <brunoroliveiira@gmail.com>
2022-06-20 23:34:37 -0300

    alterações exercicios 3

--- modelo 1/list2/src/exercises/Exercise3.java
+++ modelo 1/list2/src/exercises/Exercise3.java
@@ -8,66 +8,51 @@ public class Exercise3 {
 
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        List<String> names = new ArrayList<>();
-        List<Double> heights = new ArrayList<>();
-        List<Integer> ages = new ArrayList<>();
-        List<Double> weigths = new ArrayList<>();
-        System.out.println("Digite informações de alguns jogadores de basquete");
-        System.out.print("Nome: ");
-        String name = sc.nextLine().toUpperCase();
-        while(!name.equals("SAIR")) {
-            names.add(name);
-            System.out.print("Altura: ");
-            double height = sc.nextDouble();
-            sc.nextLine();
-            heights.add(height);
-            System.out.print("Idade: ");
-            int age = sc.nextInt();
-            sc.nextLine();
-            ages.add(age);
-            System.out.print("Peso: ");
-            double weight = sc.nextDouble();
-            sc.nextLine();
-            weigths.add(weight);
+
+        String name = "";
+        double height = 0.0;
+        int age = 0;
+        double weight = 0.0;
+        int quantityPlayers = 0;
+        double heightPlayer = 0.0;
+        int oldPlayer = 0;
+        double weightPlayer = 0.0;
+        double averageHeightPlayers = 0.0;
+        System.out.println("Digite informações sobre jogadores de basquete");
+        while (!name.equals("SAIR")){
             System.out.print("Nome: ");
             name = sc.nextLine().toUpperCase();
-        }
-        System.out.println("Quantidade de jogadores cadastrados: " + names.size());
-        double heightPlayer = 0;
-        double averageHeight = 0;
-        for (int i=0; i<heights.size(); i++){
-            averageHeight += heights.get(i);
-            if (heights.get(i) > heightPlayer){
-                heightPlayer = heights.get(i);
+            if (name.equals("SAIR")){
+                break;
+            } else {
+                quantityPlayers++;
+            }
+            System.out.print("Altura: ");
+            height = sc.nextDouble();
+            sc.nextLine();
+            averageHeightPlayers += height;
+            if (height > heightPlayer){
+                heightPlayer = height;
+            }
+            System.out.print("Idade: ");
+            age = sc.nextInt();
+            sc.nextLine();
+            if (age > oldPlayer){
+                oldPlayer = age;
+            }
+            System.out.print("Peso: ");
+            weight = sc.nextDouble();
+            sc.nextLine();
+            if (weight > weightPlayer){
+                weightPlayer = weight;
             }
         }
-        System.out.printf("Altura do jogador mais alto: %.2f%n", heightPlayer);
-        int agePlayer = 0;
-        for (int i=0; i<ages.size(); i++){
-            if (ages.get(i) > agePlayer){
-                agePlayer = ages.get(i);
-            }
-        }
-        int position = 0;
-        for (int i=0; i<ages.size(); i++){
-            if (agePlayer == ages.get(i)){
-                position = ages.indexOf(ages.get(i));
-            }
-        }
-        System.out.println("O jogador mais velho é: " + names.get(position));
-        double weightPlayer = 0;
-        for (int i=0; i<weigths.size(); i++){
-            if (weigths.get(i) > weightPlayer){
-                weightPlayer = weigths.get(i);
-            }
-        }
-        for (int i=0; i<weigths.size(); i++){
-            if (weightPlayer == weigths.get(i)){
-                position = weigths.indexOf(weigths.get(i));
-            }
-        }
-        System.out.println("O jogador mais pesado é: " + names.get(position));
-        System.out.printf("Altura média dos jogadores: %.2f%n", (averageHeight / heights.size()));
+        averageHeightPlayers /= quantityPlayers;
+        System.out.printf("Quantidade de jogadores cadastrados: %d\n", quantityPlayers);
+        System.out.printf("Altura do maior jogador: %.2f\n", heightPlayer);
+        System.out.printf("Jogador mais velho: %d anos\n", oldPlayer);
+        System.out.printf("Jogador mais pesado: %.2fKG\n", weightPlayer);
+        System.out.printf("Média das alturas dos jogadores: %.2f\n", averageHeightPlayers);
         sc.close();
     }
 }
